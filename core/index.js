70c85207f4f99c0d0f2747a99984ae0c930c8693
tenasatupitsyn <214782@bk.ru>
2020-03-20 02:39:14 -0300

    User resolve to find user id made in discord utilities

--- core/index.js
+++ core/index.js
@@ -1,3 +1,4 @@
 module.exports = {
+  DiscordUtils: require('./utils/DiscordUtils.js'),
   FileUtils: require('./utils/FileUtils.js')
 }
---
+++ core/utils/DiscordUtils.js
@@ -0,0 +1,24 @@
+module.exports = {
+  resolveUser(message, id) {
+    const members = message.guild.members.cache
+
+    if (message.mentions.members.size) {
+      id = message.mentions.members.first().id
+    } else if (!isNaN(message.parameters[0])) {
+      if (members.get(message.parameters[0])) {
+        id = members.get(message.parameters[0]).id
+      } else {
+        id = /^([0-9]){18}$/.test(message.parameters[0]) ? message.parameters[0] : message.member.id
+      }
+    } else if (isNaN(message.parameters.join(' '))) {
+      const username = message.parameters.join(' ').toLowerCase()
+      const member = members.filter(({ user }) => user.username.toLowerCase().startsWith(username)).first()
+
+      id = member ? member.id : message.member.id
+    } else {
+      id = message.member.id
+    }
+
+    return id
+  }
+}
