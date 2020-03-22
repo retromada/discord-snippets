d9caf2004c0285f2ad7f26d8ced162fe5556fa82
tenasatupitsyn <214782@bk.ru>
2020-03-22 14:03:09 -0300

    Bulk message deletion

---
+++ core/commands/moderation/prune.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'prune',
+  aliases: ['purge'],
+  description: 'Deletes existing messages on the channel',
+  usage: '[amount]',
+  category: 'moderation',
+  requirements: { paramenters: true, permissions: ['MANAGE_MESSAGES'] },
+  async execute(message) {
+    const [amount] = message.parameters
+
+    if (isNaN(amount) || amount < 1 || amount > 100) return message.channel.send('Enter a value between 1 and 100.', { code: 'fix' })
+
+    await message.delete()
+    message.channel.bulkDelete(amount)
+      .then((messages) => {
+        message.channel.send(`:wastebasket: : **${message.author.username}**, ${messages.size} message${messages.size === 1 ? '' : 's'} deleted!`)
+          .then((_message) => setTimeout(() => _message.delete(), 2555))
+      })
+      .catch((error) => {
+        message.channel.send(error.message, { code: 'fix' })
+      })
+  }
+}
