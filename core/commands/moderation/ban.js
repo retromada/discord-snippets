b29a59b1561d594417d4400502600e4ddf318b4a
tenasatupitsyn <214782@bk.ru>
2020-03-25 02:08:39 -0300

    Correction in the search for the target member and change in the message of return

--- core/commands/moderation/ban.js
+++ core/commands/moderation/ban.js
@@ -7,12 +7,13 @@ module.exports = {
   usage: '[user] <reason>',
   category: 'moderation',
   requirements: { parameters: true, permissions: ['BAN_MEMBERS'] },
-  async execute(message) {
-  	const member = await message.client.users.fetch(DiscordUtils.resolveUser(message))
+  execute(message) {
   	const reason = message.parameters.slice(1).join(' ')
 
-  	member.ban({ reason })
-  		.then((member) => message.channel.send(`${member.user.tag} was banned.\nReason: ${reason ? reason : 'None'}`))
-  		.catch((error) => message.channel.send(error.message, { code: 'fix' }))
+  	message.guild.members.ban(DiscordUtils.resolveUser(message), {
+      reason: `(Issued by ${message.author.tag})${reason ? ` ${reason}` : ''}`
+    })
+  	.then((user) => message.channel.send(`${!user.bot ? 'User' : 'Bot'} **${user.tag}** was banned. Reason: \`${reason ? reason : 'None'}\``))
+  	.catch((error) => message.channel.send(error.message, { code: 'fix' }))
   }
 }
--- core/commands/moderation/kick.js
+++ core/commands/moderation/kick.js
@@ -2,16 +2,16 @@ const { DiscordUtils } = require('../../')
 
 module.exports = {
   name: 'kick',
+  aliases: [],
   description: 'User expulsion management',
   usage: '[user] <reason>',
   category: 'moderation',
   requirements: { parameters: true, permissions: ['KICK_MEMBERS'] },
-  async execute(message) {
-  	const member = await message.client.users.fetch(DiscordUtils.resolveUser(message))
+  execute(message) {
   	const reason = message.parameters.slice(1).join(' ')
 
-  	member.kick({ reason })
-  		.then((member) => message.channel.send(`${member.user.tag} was banned.\nReason: ${reason ? reason : 'None'}`))
+  	message.guild.members.fetch(DiscordUtils.resolveUser(message)).kick(reason)
+  		.then(({ user }) => message.channel.send(`${!user.bot ? 'User' : 'Bot'} **${user.tag}** was kicked. Reason: \`${reason ? reason : 'None'}\``))
   		.catch((error) => message.channel.send(error.message, { code: 'fix' }))
   }
 }
