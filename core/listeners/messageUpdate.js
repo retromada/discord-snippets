ad4d0fc72097cdfe583a3e864246cfa8b3b35f0d
tenasatupitsyn <214782@bk.ru>
2020-03-29 21:46:19 -0300

    Sender of edited messages

---
+++ core/listeners/messageUpdate.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = (client, message, _message) => {
+  if (message.channel.id === client.notifyChannels.universal) return
+
+  message.guild.channels.cache.get(client.notifyChannels.universal).send(new MessageEmbed()
+    .setColor([255, 255, 0])
+    .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+    .addField('Channel', message.channel)
+    .addField('Before', message.content)
+    .addField('After', _message.content)
+    .setFooter(message.createdAt)
+  )
+}
