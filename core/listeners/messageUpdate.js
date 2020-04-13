fd435906a255c8e573236953e895d4204acc5854
tenasatupitsyn <214782@bk.ru>
2020-04-13 09:38:45 -0300

    Issues edits if there is an image or not

--- core/listeners/messageUpdate.js
+++ core/listeners/messageUpdate.js
@@ -3,12 +3,14 @@ const { MessageEmbed } = require('discord.js')
 module.exports = (client, message, _message) => {
   if (message.channel.id === client.notifyChannels.universal || message.content === _message.content) return
 
+  const whatContains = (message) => message.content && !message.attachments.first() ? message.content : [message.content, message.attachments.first().url].join('\n')
+
   message.guild.channels.cache.get(client.notifyChannels.universal).send(new MessageEmbed()
     .setColor([255, 255, 0])
     .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
     .addField('Channel', message.channel)
-    .addField('Before', message.content)
-    .addField('After', _message.content)
+    .addField('Before', whatContains(message))
+    .addField('After', whatContains(_message))
     .setFooter(message.createdAt)
   )
 }
