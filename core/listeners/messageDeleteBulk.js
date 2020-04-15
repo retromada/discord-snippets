48a69bb3edac8f4d292c4182e4ab14edfbd8bb55
tenasatupitsyn <214782@bk.ru>
2020-04-14 22:30:37 -0300

    Destrutorized anchor message and the file name has all the colon present in the timetable replaced by a hyphen

--- core/listeners/messageDeleteBulk.js
+++ core/listeners/messageDeleteBulk.js
@@ -1,12 +1,12 @@
 const { MessageAttachment } = require('discord.js')
 
 module.exports = (client, messages) => {
-  const anchorMessage = messages.first()
+  const { channel, guild } = messages.first()
 
-  if (anchorMessage.channel.id === client.notifyChannels.universal) return
+  if (channel.id === client.notifyChannels.universal) return
 
-  anchorMessage.guild.channels.cache.get(client.notifyChannels.universal).send(new MessageAttachment(
+  guild.channels.cache.get(client.notifyChannels.universal).send(new MessageAttachment(
     Buffer.from(messages.map((message) => `[${(new Date(message.createdAt)).toLocaleString()}] (${message.author.id}) ${message.author.tag} :: ${message.content}`).reverse().join('\n')),
-    `deleted ${anchorMessage.channel.name} ${(new Date()).toLocaleString().split(':').join('-')}.log`
+    `deleted ${channel.name} ${(new Date()).toLocaleString().replace(/:/g, '-')}.log`
   ))
 }
