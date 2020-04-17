9bea371b68169c09f790e1ee66dc54eaec764b67
tenasatupitsyn <214782@bk.ru>
2020-04-17 12:57:25 -0300

    Breaker to control message sending

---
+++ core/commands/moderation/lockdown.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: 'lockdown',
+  aliases: ['lockchannel'],
+  description: 'Toggles between allowing or blocking messages sends on the channel',
+  usage: '[on|off]',
+  category: 'moderation',
+  requirements: { parameters: true, permissions: ['MANAGE_ROLES'] },
+  async execute(message) {
+    const [breaker] = message.parameters
+
+    if (!['ON', 'OFF'].includes(breaker.toUpperCase())) return
+
+    message.channel.overwritePermissions([
+      {
+        id: '537658541859930132',
+        ...{
+          on: {
+            deny: ['SEND_MESSAGES']
+          },
+          off: {
+            allow: ['SEND_MESSAGES']
+          }
+        }[breaker.toLowerCase()]
+      }
+    ])
+  }
+}
