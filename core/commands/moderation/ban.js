3d975c497817efd3a28b601ad10947377ef6fa6f
tenasatupitsyn <214782@bk.ru>
2020-04-17 11:50:25 -0300

    Measures to prevent the member from being sterilized from the server

--- core/commands/moderation/ban.js
+++ core/commands/moderation/ban.js
@@ -8,26 +8,22 @@ module.exports = {
   category: 'moderation',
   requirements: { parameters: true, permissions: ['BAN_MEMBERS'] },
   async execute(message) {
-    try {
-      const reason = message.parameters.slice(1).join(' ')
-      const member = await message.guild.members.fetch(DiscordUtils.resolveUser(message))
+    const reason = message.parameters.slice(1).join(' ')
+    const member = await message.guild.members.fetch(DiscordUtils.resolveUser(message))
 
-      if (member) {
-        const _message = await message.channel.send(`Ban **${member.user.tag}**?`)
-        const proof = await DiscordUtils.verify(message.channel, message.author)
+    if (!member || member.id === message.author.id || member.id === message.client.user.id) return
 
-        if (proof) {
-          _message.delete()
-          member.ban({ reason: `(Issued by ${message.author.tag})${reason ? ` ${reason}` : ''}` })
-            .then(({ user }) => message.channel.send(`${!user.bot ? 'User' : 'Bot'} **${user.tag}** was banned. Reason: \`${reason ? reason : 'None'}\``))
-            .catch((error) => message.channel.send(error.message, { code: 'fix' }))
-        } else {
-          _message.delete()
-          message.channel.send('Not banned')
-        }
-      }
-    } catch (error) {
-      message.channel.send(error.message, { code: 'fix' })
+    const _message = await message.channel.send(`Ban **${member.user.tag}**?`)
+    const proof = await DiscordUtils.verify(message.channel, message.author)
+
+    if (proof) {
+      _message.delete()
+      member.ban({ reason: `(Issued by ${message.author.tag})${reason ? ` ${reason}` : ''}` })
+        .then(({ user }) => message.channel.send(`${!user.bot ? 'User' : 'Bot'} **${user.tag}** was banned. Reason: \`${reason ? reason : 'None'}\``))
+        .catch((error) => message.channel.send(error.message, { code: 'fix' }))
+    } else {
+      _message.delete()
+      message.channel.send('Not banned')
     }
   }
 }
--- core/commands/moderation/kick.js
+++ core/commands/moderation/kick.js
@@ -8,26 +8,22 @@ module.exports = {
   category: 'moderation',
   requirements: { parameters: true, permissions: ['KICK_MEMBERS'] },
   async execute(message) {
-    try {
-      const reason = message.parameters.slice(1).join(' ')
-      const member = await message.guild.members.fetch(DiscordUtils.resolveUser(message))
+    const reason = message.parameters.slice(1).join(' ')
+    const member = await message.guild.members.fetch(DiscordUtils.resolveUser(message))
 
-      if (member) {
-        const _message = await message.channel.send(`Kick **${member.user.tag}**?`)
-        const proof = await DiscordUtils.verify(message.channel, message.author)
+    if (!member || member.id === message.author.id || member.id === message.client.user.id) return
 
-        if (proof) {
-          _message.delete()
-          member.kick(reason)
-            .then(({ user }) => message.channel.send(`${!user.bot ? 'User' : 'Bot'} **${user.tag}** was kicked. Reason: \`${reason ? reason : 'None'}\``))
-            .catch((error) => message.channel.send(error.message, { code: 'fix' }))
-        } else {
-          _message.delete()
-          message.channel.send('Not kicked')
-        }
-      }
-    } catch (error) {
-      message.channel.send(error.message, { code: 'fix' })
+    const _message = await message.channel.send(`Kick **${member.user.tag}**?`)
+    const proof = await DiscordUtils.verify(message.channel, message.author)
+
+    if (proof) {
+      _message.delete()
+      member.kick(reason)
+        .then(({ user }) => message.channel.send(`${!user.bot ? 'User' : 'Bot'} **${user.tag}** was kicked. Reason: \`${reason ? reason : 'None'}\``))
+        .catch((error) => message.channel.send(error.message, { code: 'fix' }))
+    } else {
+      _message.delete()
+      message.channel.send('Not kicked')
     }
   }
 }
