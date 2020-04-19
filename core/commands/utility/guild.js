7ff2c23aba2a771de79a5acf79c235b9e1e6518e
tenasatupitsyn <214782@bk.ru>
2020-04-19 06:01:10 -0300

    General corrections including it to the standard

--- core/commands/utility/guild.js
+++ core/commands/utility/guild.js
@@ -2,41 +2,65 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = {
   name: 'guild',
-  aliases: ['server', 'sv', 'guild-info', 'server-info'],
-  description: 'Shows detailed information about the server',
+  aliases: [
+    'ginfo',
+    'guildinfo',
+    'server',
+    'sinfo',
+    'serverinfo'
+  ],
+  description: 'Shows details about the server',
   category: 'utility',
   execute(message) {
-    message.guild.fetch(message.guild)
-    .then((guild) => {
-      message.channel.send(new MessageEmbed()
-        .setAuthor(guild.name, guild.iconURL({ format: 'png', dynamic: true, size: 64 }))
-        .setThumbnail(guild.iconURL({ format: 'png', dynamic: true, size: 256 }))
-        .addFields([
-          {
-            name: 'Information',
-            value: `Id: ${guild.id}\nAcronym: ${guild.nameAcronym}\n Created: ${guild.createdAt}\n Owner: ${guild.owner}\n Region: ${guild.region}`,
-            inline: true
-          },
-          {
-            name: 'Moderation',
-            value: `AFK Timeout: ${guild.afkTimeout}\n MFA: ${guild.mfaLevel}\n Content Filter: ${guild.explicitContentFilter}\n Verification: ${guild.verificationLevel}`,
-            inline: true
-          },
-          {
-            name: 'Channels',
-            value: `System: ${guild.systemChannel}\n Widget: ${guild.widgetChannel ? widget : 'None'}`,
-            inline: false
-          },
-          {
-            name: 'Counts',
-            value: `Members: ${guild.members.cache.size}\n Channels: ${guild.channels.cache.size}\n  -[Category]: ${guild.channels.cache.filter(({ type }) => type === 'category').size}
-            -[Text]: ${guild.channels.cache.filter(({ type }) => type === 'text').size}\n  -[Voice]: ${guild.channels.cache.filter(({ type }) => type === 'voice').size}
-            Emojis: ${guild.emojis.cache.size}\n  -[Static]: ${guild.emojis.cache.filter(e => !e.animated).size}\n  -[Animated]: ${guild.emojis.cache.filter(e => e.animated).size}`,
-            inline: true
-          }
-        ])
-      )
-    })
-    .catch((error) => message.channel.send(error.message, { code: 'fix' }))
+    const guild = message.guild
+    const channels = guild.channels.cache
+    const emojis = guild.emojis.cache
+
+    message.channel.send(new MessageEmbed()
+      .setAuthor(guild.name, guild.iconURL({ format: 'png', dynamic: true, size: 64 }))
+      .setThumbnail(guild.iconURL({ format: 'png', dynamic: true, size: 256 }))
+      .addFields([
+        {
+          name: 'Information',
+          value: [
+            `Id: ${guild.id}`,
+            `Acronym: ${guild.nameAcronym}`,
+            `Created: ${guild.createdAt}`,
+            `Owner: ${guild.owner}`,
+            `Region: ${guild.region}`
+          ].join('\n')
+        },
+        {
+          name: 'Moderation',
+          value: [
+            `AFK Timeout: ${guild.afkTimeout}`,
+            `MFA: ${guild.mfaLevel}`,
+            `Content Filter: ${guild.explicitContentFilter}`,
+            `Verification: ${guild.verificationLevel}`
+          ].join('\n')
+        },
+        {
+          name: 'Channels',
+          value: [
+            `System: ${guild.systemChannel}`,
+            `Widget: ${guild.widgetChannel ? guild.widgetChannel : 'None'}`
+          ].join('\n'),
+          inline: false
+        },
+        {
+          name: 'Counts',
+          value: [
+            `Members: ${guild.members.cache.size}`,
+            `Channels: ${channels.size}`,
+            `- [Category]: ${channels.filter(({ type }) => type === 'category').size}`,
+            `- [Text]: ${channels.filter(({ type }) => type === 'text').size}`,
+            `- [Voice]: ${channels.filter(({ type }) => type === 'voice').size}`,
+            `Emojis: ${emojis.size}`,
+            `- [Static]: ${emojis.filter(({ animated }) => !animated).size}`,
+            `- [Animated]: ${emojis.filter(({ animated }) => animated).size}`
+          ].join('\n')
+        }
+      ].map((element) => element.inline !== false ? ({ ...element, inline: true }) : element))
+    )
   }
 }
