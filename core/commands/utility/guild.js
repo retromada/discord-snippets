0a5b5ea61cd412cc3c7cf77708bdadbd50739bef
tenasatupitsyn <214782@bk.ru>
2020-04-19 06:34:43 -0300

    Boost, Shard and Roles Information

--- core/commands/utility/guild.js
+++ core/commands/utility/guild.js
@@ -15,6 +15,7 @@ module.exports = {
     const guild = message.guild
     const channels = guild.channels.cache
     const emojis = guild.emojis.cache
+    const roles = guild.roles.cache
 
     message.channel.send(new MessageEmbed()
       .setAuthor(guild.name, guild.iconURL({ format: 'png', dynamic: true, size: 64 }))
@@ -26,11 +27,12 @@ module.exports = {
             `Id: ${guild.id}`,
             `Acronym: ${guild.nameAcronym}`,
             `Created: ${guild.createdAt}`,
+            `Nitro Tier: ${guild.premiumTier}`,
             `Owner: ${guild.owner}`,
-            `Region: ${guild.region}`
+            `Region: ${guild.region}`,
+            `Shard: ${guild.shardID + 1}/${guild.shard.manager.shards.size}`
           ].join('\n')
-        },
-        {
+        }, {
           name: 'Moderation',
           value: [
             `AFK Timeout: ${guild.afkTimeout}`,
@@ -38,19 +40,17 @@ module.exports = {
             `Content Filter: ${guild.explicitContentFilter}`,
             `Verification: ${guild.verificationLevel}`
           ].join('\n')
-        },
-        {
+        }, {
           name: 'Channels',
           value: [
             `System: ${guild.systemChannel}`,
             `Widget: ${guild.widgetChannel ? guild.widgetChannel : 'None'}`
           ].join('\n'),
           inline: false
-        },
-        {
+        }, {
           name: 'Counts',
           value: [
-            `Members: ${guild.members.cache.size}`,
+            `Members: ${guild.memberCount}`,
             `Channels: ${channels.size}`,
             `- [Category]: ${channels.filter(({ type }) => type === 'category').size}`,
             `- [Text]: ${channels.filter(({ type }) => type === 'text').size}`,
@@ -58,6 +58,14 @@ module.exports = {
             `Emojis: ${emojis.size}`,
             `- [Static]: ${emojis.filter(({ animated }) => !animated).size}`,
             `- [Animated]: ${emojis.filter(({ animated }) => animated).size}`
+          ].join('\n')
+        }, {
+          name: '­',
+          value: [
+            `Boosts: ${guild.premiumSubscriptionCount ? guild.premiumSubscriptionCount : 0}`,
+            `Roles: ${roles.size}`,
+            `- [Unmanaged]: ${roles.filter(({ managed }) => !managed).size}`,
+            `- [Managed]: ${roles.filter(({ managed }) => managed).size}`
           ].join('\n')
         }
       ].map((element) => element.inline !== false ? ({ ...element, inline: true }) : element))
