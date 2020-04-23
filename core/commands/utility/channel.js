47899576d01e2d77bf54798c3f7a9759276c9585
tenasatupitsyn <214782@bk.ru>
2020-04-23 03:44:48 -0300

    Possibility to obtain information from other server channels if provided

--- core/commands/utility/channel.js
+++ core/commands/utility/channel.js
@@ -4,13 +4,31 @@ module.exports = {
   name: 'channel',
   aliases: ['cinfo', 'channelinfo'],
   description: 'Shows details about the channel',
+  usage: '<channel>',
   category: 'utility',
-  execute(message) {
-    const channel = message.channel
+  execute(message, channelID) {
+    if (message.mentions.channels.size) {
+      channelID = message.mentions.channels.first().id
+    } else if (isNaN(message.parameters.join('-'))) {
+      const channelName = message.parameters.join('-').toLowerCase()
+      const channel = message.guild.channels.cache.find((channel) => channel.name.includes(channelName))
 
-    channel.send(new MessageEmbed()
+      channelID = channel ? channel.id : message.channel.id
+    } else if (!isNaN(message.parameters[0])) {
+      channelID = message.parameters[0]
+    } else {
+      channelID = message.channel.id
+    }
+
+    const channels = message.guild.channels.cache
+
+    if (!channels.has(channelID)) return
+
+    const channel = channels.get(channelID)
+
+    message.channel.send(new MessageEmbed()
       .setTitle(channel.name)
-      .setDescription(message.channel)
+      .setDescription(channel)
       .addFields([
         {
           name: 'ID',
@@ -21,7 +39,7 @@ module.exports = {
           value: channel.type
         }, {
           name: 'NSFW',
-          value: channel.nsfw
+          value: channel.nsfw ? 'Enabled' : 'Disabled'
         }, {
           name: 'Creation Date',
           value: channel.createdAt
