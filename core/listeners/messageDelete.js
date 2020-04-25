78876bb0e912af483422588d3a7e43f52ecd0640
tenasatupitsyn <214782@bk.ru>
2020-04-25 19:53:28 -0300

    Content handling if it contains embed

--- core/listeners/messageDelete.js
+++ core/listeners/messageDelete.js
@@ -10,8 +10,15 @@ module.exports = (client, message) => {
     .addField('Content',
       message.content && !message.attachments.first()
       ? message.content
-      : [message.content, message.attachments.first().url].join('\n')
+      : !message.embeds.length
+        ? [message.content, message.attachments.first().url].join('\n')
+        : (message.embeds
+            .forEach((embed) => Object.keys(embed).forEach((key) => (embed[key] === undefined || embed[key] === null || !embed[key].length) && delete embed[key])),
+           message.embeds
+            .map((embed) => Object.entries(embed).map(([key, value]) => `**${key}**: ${value}`).join('\n'))
+            .join('\n\n')
+          )
     )
     .setFooter(message.createdAt)
-  ).catch((error) => client.log(error))
+  )
 }
