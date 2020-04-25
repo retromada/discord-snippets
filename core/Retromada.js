b446d35184032380846c4610fe71afa5eab3b0b2
tenasatupitsyn <214782@bk.ru>
2020-04-25 19:15:15 -0300

    Removing content from the main snippet

--- core/Retromada.js
+++ core/Retromada.js
@@ -1,21 +1,11 @@
 const { Client } = require('discord.js')
 const Loaders = require('./loaders')
-const {
-  NOTIFY_JOIN_LEAVE_CHANNEL_ID,
-  NOTIFY_BAN_UNBAN_CHANNEL_ID,
-  NOTIFY_UNIVERSAL_CHANNEL_ID
-} = process.env
 
 module.exports = class Retromada extends Client {
   constructor(options) {
     super(options)
 
     this.queue = new Map()
-    this.notifyChannels = {
-      joinAndLeave: NOTIFY_JOIN_LEAVE_CHANNEL_ID,
-      banAndUnban: NOTIFY_BAN_UNBAN_CHANNEL_ID,
-      universal: NOTIFY_UNIVERSAL_CHANNEL_ID
-    }
 
     this.initializeLoaders()
   }
