19b1174e65587b1f8f65d6717390f4aff304a2cd
meggatrony <hyossha@gmail.com>
2020-04-27 20:24:05 -0300

    Standardizing conditions

--- core/commands/music/song.js
+++ core/commands/music/song.js
@@ -8,12 +8,12 @@ module.exports = {
   execute(message) {
     const queue = message.client.queue.get(message.guild.id)
 
-    if (!queue || !queue.songs.length) return message.channel.send(new MessageEmbed()
+    if (!queue || (queue && !queue.songs.length)) return message.channel.send(new MessageEmbed()
       .setColor([255, 0, 0])
       .setDescription('There is no song playing!')
     )
 
-    const song = queue.songs.shift()
+    const song = queue.songs[0]
 
     message.channel.send(new MessageEmbed()
       .setTitle('Currently playing')
