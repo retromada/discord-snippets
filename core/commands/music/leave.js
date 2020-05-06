6eb90b7389615c803350c04828311db5f714d22d
tenasatupitsyn <214782@bk.ru>
2020-05-06 04:57:50 -0300

    Checking if the bot is on the voice channel to proceed

--- core/commands/music/leave.js
+++ core/commands/music/leave.js
@@ -7,6 +7,8 @@ module.exports = {
   category: 'music',
   requirements: { needVoiceChannel: true },
   execute(message) {
+    if (!message.member.voice.channel.members.has(message.client.user.id)) return
+
     const player = message.client.player.get(message.guild.id)
     player.tracks = []
     player.voice.leave()
